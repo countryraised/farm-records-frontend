259eff87e4834d0062a9264bb117e7fd92955e94
vincentm <matthiasvincentm@johndeere.com>
2021-03-11 21:03:32 -0600

    retrieving data from backend through api call and presenting it on app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      App.js page
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      records: [],
+
+    };
+  }
+
+  componentDidMount = () => {
+    this.getRecords();
+  };
+  getRecords = async () => {
+    const response = await axios.get('http://localhost:3001/farmrecord/all');
+    console.log(response);
+    this.setState({
+      records: response.data,
+    });
+    console.log(this.state.records);
+  };
+
+  render () {  
+    const records = this.state.records.map((record) => {
+      return (
+        <div>
+          <h4>{record.fieldName}</h4>          
+          <p>
+            {record.dateComplete}<br/>
+            {record.details}
+          </p>
+        </div>
+      );
+    }); 
+      return (
+        <div className="App">
+          <h3>App.js page</h3> 
+
+          <div>{records}</div>
+          
+
+        </div>
+      );    
+  }
 }
+
 
 export default App;
