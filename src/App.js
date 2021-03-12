e495e8a872308d654d74548f2c7f6e5db32cbb09
vincentm <matthiasvincentm@johndeere.com>
2021-03-12 16:00:36 -0600

    adding user login and authorization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import Homepage from './components/Homepage';
+import { Route, Link, Redirect } from 'react-router-dom';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       records: [],
-
+      userProfile:{},
+      username: '',
+      password: '',
     };
   }
 
   componentDidMount = () => {
     this.getRecords();
+    // this.getUser();
   };
   getRecords = async () => {
     const response = await axios.get('http://localhost:3001/farmrecord/all');
-    console.log(response);
+    // console.log(response);
     this.setState({
       records: response.data,
     });
-    console.log(this.state.records);
+    // console.log(this.state.records);
+  };
+  // getUser = async () => {
+  //   const response = await axios.get('http://localhost:3001/user/profile/1');
+  //   console.log(response);
+  //   this.setState({
+  //     userProfile: response.data,
+  //   });
+  //   console.log(this.state.userProfile);
+  // };
+
+  loginOnChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  login = async (e) => {
+    e.preventDefault();
+    const data = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    console.log("i am running")
+    const response = await axios.post('http://localhost:3001/auth/login', data);
+    console.log(response);
+    this.setState({
+      userProfile: response.data,
+    });
   };
 
   render () {  
@@ -38,6 +73,15 @@ class App extends Component {
       return (
         <div className="App">
           <h3>App.js page</h3> 
+
+          <Route exact path="/" render = {() => (
+              <Homepage 
+                login={this.state.login}
+                loginOnChange={this.state.loginOnChange}/>
+          )} 
+          />
+          
+          
 
           <div>{records}</div>
           
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,8 +3,29 @@ import React from 'react';
 function Homepage(props) {
     return(
         <div>
+            <h1>Farm Records App</h1>
             {/* user login */}
+            <form onSubmit={props.login}>
+                <input
+                    name='username'
+                    type='text'
+                    placeholder='username'
+                    value={props.username}
+                    onChange={props.loginOnChange}
+                />
+                <input
+                    name='password'
+                    type='password'
+                    placeholder='password'
+                    value={props.password}
+                    onChange={props.loginOnChange}
+                />
+                <input type='submit' value='Login' />
+            </form>
+
             {/* user sign up */}
+
+
         </div>
     )
 }
